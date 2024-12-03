cf150bc0c61866698fd3fd2f65089f864ee38135
joycetan913 <joycetanyy913@gmail.com>
2024-12-03 14:11:26 +0800

    initial commit

---
+++ src/main/java/vttp_batchb/practice_test/PracticeTestApplication.java
@@ -0,0 +1,13 @@
+package vttp_batchb.practice_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vttp_batchb/practice_test/PracticeTestApplicationTests.java
@@ -0,0 +1,13 @@
+package vttp_batchb.practice_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
